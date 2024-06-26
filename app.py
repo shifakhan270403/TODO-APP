4db32e72ab55a6702aba36dc952e45ae04ecfe0b
shifakhan270403 <shifakhan.work@gmail.com>
2024-06-27 00:17:36 +0530

    Made Changes !!!

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:/Users/shifa khan/OneDrive/Documents/PROJECTS/TODO_FLASK/instance/todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
